932533dfd604417b11fd9005c2a06178a062429b
Mahmoud <mahmoud@zalt.me>
2018-05-21 00:09:11 +0300

    changes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -12,5 +13,27 @@ class PostsController extends Controller
 
     public function show() {
         return view('posts.show');
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store() {
+
+
+        // create a new post using the request data
+
+        $post = new Post;
+        $post->title = request('title');
+        $post->body = request('body');
+
+        // save to the database
+
+        $post->save();
+
+        // redirect it to some page 
+
+        return redirect('/');
     }
 }
